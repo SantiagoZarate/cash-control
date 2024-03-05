9e286a6f8f8d5dc6984a15f689a5d6550dfed551
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-05 18:52:47 -0300

    style: style RecentMovementsLoader

---
+++ src/pages/homePage/recentMovements/RecentMovementsLoader.tsx
@@ -0,0 +1,17 @@
+import { Loader } from "@component";
+import "./RecentMovement.styles.css";
+
+export function RecentMovementsLoader() {
+  return (
+    <div
+      id="recent-movement-loader"
+      className="relative overflow-hidden min-h-56 flex items-center justify-center bg-card rounded-xl"
+    >
+      <div className="flex flex-col gap-4">
+        <Loader />
+        <p className="text-xs capitalize">lets see what we've got here...</p>
+      </div>
+      <div className="absolute pointer-events-none inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]" />
+    </div>
+  );
+}
