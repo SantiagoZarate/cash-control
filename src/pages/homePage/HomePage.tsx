3ef6773c421ef94e55dfc0cdbd0068a80ca8c246
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-06 14:29:59 -0300

    feat: add header on HomePage

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./header/Header";
 import { Hero } from "./hero/Hero";
 import { RecentMovements } from "./recentMovements/RecentMovements";
 
 export function HomePage() {
   return (
     <section className="relative flex flex-col divide-y divide-border ">
+      <Header />
       <Hero />
       <RecentMovements />
     </section>
